a727be6a46c631cc3ba5b02ee226b737af704cb3
Lee Guiberson <lee.guiberson@gmail.com>
2022-06-16 11:52:15 -0600

    Revise comments for Input class

--- engine/Input.java
+++ engine/Input.java
@@ -5,15 +5,20 @@ import java.util.ArrayList;
 import java.awt.event.KeyListener;
 import java.awt.event.KeyEvent;
 
-// Class that handles all of the input
+/**
+ * Class that handles all of the input
+ */
 public class Input implements KeyListener {
-   // Hashmap of all of the keycodes to the lists of Controllable objects that need the key with the respective key code
+   /** Hash map of keycodes to objects that they affect  */
    HashMap<Integer, ArrayList<Controllable>> keysToControllables;
-      
+
+   /**
+    * Initializes input object
+    * @param objects Hashmap of objects
+    */
    public Input(HashMap<String, Object> objects) {
-      // Put all of the Controllable objects in hashmap of objects into list
+      // Get arraylist of Controllable objects
       ArrayList<Controllable> controllables = new ArrayList<Controllable>();
-      
       for(Object o : objects.values())
          if(o instanceof Controllable)
             controllables.add((Controllable) o);
@@ -21,38 +26,37 @@ public class Input implements KeyListener {
       // Associate all the keys with the controllable objects that they can correspond to
       keysToControllables = new HashMap<Integer, ArrayList<Controllable>>();
       
-      // Iterate through the array list of controllables
+      // Iterate through everykeycode
       for(Controllable c : controllables) {
-         // Iterate throug the keycodes corresponding to the Controllable object
          for(int keyNum : c.getInputs()) {
-            // Get array list of Controllables associated with key or create one if none
-            ArrayList<Controllable> controllablesAssociatedWithKey = keysToControllables.computeIfAbsent(keyNum, k -> new ArrayList<Controllable>());
-            // Add the current controllable to the list corresponding to the key
+            // Add respective Controllable to keycode index
+            ArrayList<Controllable> controllablesAssociatedWithKey = 
+               keysToControllables.computeIfAbsent(keyNum, k -> new ArrayList<Controllable>());
             controllablesAssociatedWithKey.add(c);
          }
       }   
    }
    
-   // 
+   /**
+    * Registers the input
+    * @param e KeyEvent object
+    * @param t KeyEventType enum
+    */
    private void register(KeyEvent e, KeyEventType t) {
-      // Get the list of Controllables corresponding to a key code
+      // Register input for each Controllable object that responds to keycode
       ArrayList<Controllable> controllables = keysToControllables.get(e.getKeyCode());
-      // If it exist, iterate through it and send input to each Controllable
       if(controllables != null) for(Controllable c : controllables)
          c.registerInput(e, t);                           
    }    
    
-   // Runs if key is released
    public void keyReleased(KeyEvent e) {
       register(e, KeyEventType.RELEASED);
    }
    
-   // Runs if key is pressed
    public void keyPressed(KeyEvent e) {
       register(e, KeyEventType.PRESSED);
    }
    
-   // Runs if key is typed
    public void keyTyped(KeyEvent e) {
      register(e, KeyEventType.TYPED);
    }
